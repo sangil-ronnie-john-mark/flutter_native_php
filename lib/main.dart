79ba3ce2862cdf89850efb62df0b5b7dc3bcc5e2
ronniejohnmarksangil <ronniejohnmarksangil@gmail.com>
2026-09-09 08:53:42 +0800

    "Flutter Pub Add Liquid Glass Widgets Library"

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
-void main(){
-  print("Hello World");
+import 'package:flutter/cupertino.dart';
+import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LiquidGlassWidgets.initialize();
+  runApp(LiquidGlassWidgets.wrap(child: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
